e86d4d64eaa41ebdbc8043def2645a7f884de094
Eric J Fisher <EricJFisher@users.noreply.github.com>
2020-04-05 16:10:36 -0400

    Add project files.

---
+++ BGSBuddyTests/Repositories/EddbIoRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Repositories;
+using Xunit;
+
+namespace BGSBuddyTests.Repositories
+{
+    public class EddbIoRepositoryTests
+    {
+        public EddbIoRepository repository = new EddbIoRepository();
+
+        [Fact]
+        public void FetchDataTest()
+        {
+            
+        }
+    }
+}
