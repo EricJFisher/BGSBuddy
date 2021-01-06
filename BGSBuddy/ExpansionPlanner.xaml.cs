04c3fdd5258c169db65dae8f3983b635083ebf39
Eric J Fisher <EricJFisher@users.noreply.github.com>
2021-01-06 01:09:21 -0500

    Backed off of web application, created Expansion Planning functionality and several minor improvements.

---
+++ BGSBuddy/ExpansionPlanner.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace BGSBuddy
+{
+    public partial class ExpansionPlanner : Page
+    {
+        public ExpansionPlanner()
+        {
+            InitializeComponent();
+        }
+    }
+}
